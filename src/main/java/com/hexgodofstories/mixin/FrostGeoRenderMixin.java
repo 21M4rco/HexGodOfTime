57b159eadda6b1bd01612d457f0db62df67f0afa
21M4rco <132926304+21M4rco@users.noreply.github.com>
2026-09-23 22:10:01 +0100

    Hook GeckoLib default texture lookup without Minecraft remapping

--- src/main/java/com/hexgodofstories/mixin/FrostGeoRenderMixin.java
+++ src/main/java/com/hexgodofstories/mixin/FrostGeoRenderMixin.java
@@ -7,15 +7,16 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.core.animatable.GeoAnimatable;
+import software.bernie.geckolib.renderer.GeoRenderer;
 
 /** GeckoLib draws many modded mobs without LivingEntityRenderer; keep their own GeoModel too. */
-@Mixin(GeoEntityRenderer.class)
-public abstract class FrostGeoRenderMixin {
-    private static final ResourceLocation HGOS_ICE=new ResourceLocation("minecraft","textures/block/ice.png");
+@Mixin(GeoRenderer.class)
+public interface FrostGeoRenderMixin {
+    ResourceLocation HGOS_ICE=new ResourceLocation("minecraft","textures/block/ice.png");
 
-    @Inject(method="getTextureLocation",at=@At("HEAD"),cancellable=true)
-    private void hgos$geoIce(Entity entity,CallbackInfoReturnable<ResourceLocation> cir) {
-        if(FrostClient.frozen(entity.getId()))cir.setReturnValue(HGOS_ICE);
+    @Inject(method="getTextureLocation",at=@At("HEAD"),cancellable=true,remap=false)
+    private void hgos$geoIce(GeoAnimatable animatable,CallbackInfoReturnable<ResourceLocation> cir) {
+        if(animatable instanceof Entity entity&&FrostClient.frozen(entity.getId()))cir.setReturnValue(HGOS_ICE);
     }
 }
